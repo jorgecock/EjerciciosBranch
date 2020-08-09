a6584b8ebda3f6134d3905685d37ecdecd0e625a
Jorge A. Cock <jorgecock@gmail.com>
2020-08-08 19:03:19 -0500

    V0.1

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
-	echo("Hola");
+	echo("Hola <br><br>");
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Ejemplo para ejercicios Branch</title>
+	<LINK REL=StyleSheet HREF="style.css" TYPE="text/css">
+
 </head>
 <body>
-
+	<h1>Texto de ejemplo</h1>
 </body>
 </html>
